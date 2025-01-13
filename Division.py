f36baeab5d2e9f1f8b8c5c162094cfa41cce4e85
Adi280513 <adi.gupta22@st.niituniversity.in>
2025-01-13 23:39:09 +0530

    corrected div

--- Division.py
+++ Division.py
@@ -1,4 +1,10 @@
 # This function adds two numbers
 def divide(x, y):
-    return x / y
-
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        return "Error: Division by zero is not allowed."
+    except TypeError:
+        return "Error: Both inputs must be numbers."
+    else:
+        return result
